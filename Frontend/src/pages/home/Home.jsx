0690f6a884a19b9607af712deeb64a3bd5d9668d
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2023-03-21 12:22:30 -0400

    temp

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import "./Header.css"; // import the CSS file for styling
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="search-bar">
+        {/* left-aligned search bar */}
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="center-icons">
+        {/* 3 centered squares */}
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+      </div>
+      <div className="right-icons">
+        {/* 3 circles */}
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+      </div>
+    </header>
+  );
+};
 
 const Home = () => {
-  return <div>Hello, World!</div>;
+  return (
+    <div>
+      <Header />
+      <div>Hello, World!</div>
+    </div>
+  );
 };
 
 export default Home;
