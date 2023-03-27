71431c7ecbdf9c7dbb39c80923cecdc5e8453645
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2023-03-27 15:18:08 -0400

    Update People.jsx

--- Frontend/app/People.jsx
+++ Frontend/app/People.jsx
@@ -5,7 +5,65 @@ import Header from "../components/Header";
 import DropDownMenu from "../components/DropDownMenu";
 import ProfilesListItem from "../components/ProfileListItem";
 
+const data = [
+  {
+    key: "anthonyV@uwaterloo.ca",
+    firstName: "Anthony",
+    lastName: "Vasquez",
+    termInfo: "2B Computer Science",
+  },
+  {
+    key: "djon1234@uwaterloo.ca",
+    firstName: "Jon",
+    lastName: "Doe",
+    termInfo: "2A Computer Science",
+  },
+  {
+    key: "djane123@uwaterloo.ca",
+    firstName: "Jane",
+    lastName: "Doe",
+    termInfo: "2A Computer Science",
+  },
+  {
+    key: "mlkjr420@uwaterloo.ca",
+    firstName: "Martin Luther",
+    lastName: "King Jr",
+    termInfo: "4A Legal Studies",
+  },
+  {
+    key: "bwayne12@uwaterloo.ca",
+    firstName: "Bruce",
+    lastName: "Wayne",
+    termInfo: "3A Math Business and Physics Joint",
+  },
+  {
+    key: "ckent123@uwaterloo.ca",
+    firstName: "Clark",
+    lastName: "Kent",
+    termInfo: "4A English",
+  },
+  {
+    key: "ballen12@uwaterloo.ca",
+    firstName: "Bary",
+    lastName: "Allen",
+    termInfo: "1A Criminology",
+  },
+  {
+    key: "oqueen12@uwaterloo.ca",
+    firstName: "Oliver",
+    lastName: "Queen",
+    termInfo: "4B Policial Science",
+  },
+];
+
 const Page = () => {
+  const handleProfileClick = (index) => {
+    console.log(index);
+    console.log(data[index]);
+    console.log("Going to View Profile Screen");
+    console.log("----------------------------");
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
@@ -15,36 +73,19 @@ const Page = () => {
       <View style={styles.body}>
         <View style={styles.contentBody}>
           <DropDownMenu />
-
-          <TouchableOpacity>
-            <ProfilesListItem
-              FirstName="Jon"
-              LastName="Doe"
-              TermInfo="2B Computer Science"
-            />
-          </TouchableOpacity>
-
-          <TouchableOpacity>
-            <ProfilesListItem
-              FirstName="Jane"
-              LastName="Doe"
-              TermInfo="2A Honors Mathematics"
-            />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <ProfilesListItem
-              FirstName="Brian"
-              LastName="Dong"
-              TermInfo="SWE Intern @ TD Canada Trust"
-            />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <ProfilesListItem
-              FirstName="Rahul"
-              LastName="Gandhi"
-              TermInfo="Data Scientist Intern @ Amazon"
-            />
-          </TouchableOpacity>
+          {data.map((person, index) => (
+            <TouchableOpacity
+              onPress={() => {
+                handleProfileClick(index);
+              }}
+            >
+              <ProfilesListItem
+                FirstName={person.firstName}
+                LastName={person.lastName}
+                TermInfo={person.termInfo}
+              />
+            </TouchableOpacity>
+          ))}
         </View>
       </View>
     </View>
