59e44fa6584ec88435fc2afd8a9f939736a1dd47
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2023-03-27 16:23:37 -0400

    Update ViewSelfProfile.jsx

--- Frontend/app/ViewSelfProfile.jsx
+++ Frontend/app/ViewSelfProfile.jsx
@@ -1,61 +1,92 @@
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Linking,
+} from "react-native";
 import { Link } from "expo-router";
 
 import Header from "../components/Header";
 import TimeLine from "../components/TimeLine";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-data = [
-  {
-    type: "study",
-    term: "1A",
-    termDescription: "Math 135, Math 137, CS 135, Phys 121, SPCOM 100",
-    startDate: "Sept 2020",
-    endDate: "Dec 2020",
-  },
-  {
-    type: "study",
-    term: "1B ",
-    termDescription: "Math 136, Math 138, CS 136, AFM 101, Econ 101",
-    startDate: "Jan 2021",
-    endDate: "Apr 2021",
-  },
-  {
-    type: "work",
-    term: "Coop 1",
-    termDescription: "Software Developer @ ABC Inc.",
-    startDate: "May 2021",
-    endDate: "Aug 2021",
-  },
-  {
-    type: "study",
-    term: "2A",
-    termDescription: "CS 245, CS 246, Math 239, Stat 230, AFM 102",
-    startDate: "Sep 2021",
-    endDate: "Dec 2021",
-  },
-  {
-    type: "off",
-    term: "Off-Term",
-    termDescription: "I took a break!",
-    startDate: "Jan 2022",
-    endDate: "April 2022",
-  },
-  {
-    type: "work",
-    term: "Coop 2",
-    termDescription: "Software Developer @ DEF Inc.",
-    startDate: "May 2022",
-    endDate: "Aug 2022",
-  },
-  {
-    type: "study",
-    term: "2B",
-    termDescription: "CS 241, CS 251, CS 240, SPCOM 200, Stat 231 ",
-    startDate: "Sept 2022",
-    endDate: "Dec 2022",
-  },
-];
-
+data = {
+  firstName: "Jane",
+  lastName: "Doe",
+  description:
+    "Hi there, I'm John. I'm an international student originally from Argentina, and I'm from currently studying CS.",
+  timeLine: [
+    {
+      type: "study",
+      term: "1A",
+      termDescription: "Math 135, Math 137, CS 135, Phys 121, SPCOM 100",
+      startDate: "Sept 2020",
+      endDate: "Dec 2020",
+    },
+    {
+      type: "study",
+      term: "1B ",
+      termDescription: "Math 136, Math 138, CS 136, AFM 101, Econ 101",
+      startDate: "Jan 2021",
+      endDate: "Apr 2021",
+    },
+    {
+      type: "work",
+      term: "Coop 1",
+      termDescription: "Software Developer @ ABC Inc.",
+      startDate: "May 2021",
+      endDate: "Aug 2021",
+    },
+    {
+      type: "study",
+      term: "2A",
+      termDescription: "CS 245, CS 246, Math 239, Stat 230, AFM 102",
+      startDate: "Sep 2021",
+      endDate: "Dec 2021",
+    },
+    {
+      type: "off",
+      term: "Off-Term",
+      termDescription: "I took a break!",
+      startDate: "Jan 2022",
+      endDate: "April 2022",
+    },
+    {
+      type: "work",
+      term: "Coop 2",
+      termDescription: "Software Developer @ DEF Inc.",
+      startDate: "May 2022",
+      endDate: "Aug 2022",
+    },
+    {
+      type: "study",
+      term: "2B",
+      termDescription: "CS 241, CS 251, CS 240, SPCOM 200, Stat 231 ",
+      startDate: "Sept 2022",
+      endDate: "Dec 2022",
+    },
+  ],
+  links: [
+    {
+      type: "LinkedIn",
+      link: "www.linkedin.com",
+    },
+    {
+      type: "Email",
+      link: "email@gmail.com",
+    },
+    {
+      type: "GitHub",
+      link: "www.github.com",
+    },
+    {
+      type: "Discord",
+      link: "www.discord.com",
+    },
+  ],
+};
 const ViewSelfProfile = () => {
   return (
     <View style={styles.container}>
@@ -74,20 +105,27 @@ const ViewSelfProfile = () => {
               alignSelf: "center",
             }}
           >
-            <Text style={styles.title}>John Doe</Text>
-            <Text style={styles.subtitle}>
-              {
-                "Hi there, I'm John. I'm an international student originally from Argentina, and I'm from currently studying CS."
-              }
+            <Text style={styles.title}>
+              {data.firstName + " " + data.lastName}
             </Text>
+            <Text style={styles.subtitle}>{data.description}</Text>
 
-            <TimeLine data={data} />
+            <TimeLine data={data.timeLine} />
 
             <View style={styles.linksContainer}>
-              <Text style={styles.link}>LinkedIn</Text>
+              {data.links.map((linkInfo) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    Linking.openURL(linkInfo.link);
+                  }}
+                >
+                  <Text style={styles.link}>{linkInfo.title}</Text>
+                </TouchableOpacity>
+              ))}
+              {/* <Text style={styles.link}>LinkedIn</Text>
               <Text style={styles.link}>GitHub</Text>
               <Text style={styles.link}>Discord</Text>
-              <Text style={styles.link}>Email</Text>
+              <Text style={styles.link}>Email</Text> */}
             </View>
           </ScrollView>
         </View>
