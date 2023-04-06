1f8f6cf08e5b8e04b51c12338e2754b34a501c3e
Vansh Mehta <61158808+vanshvmehta@users.noreply.github.com>
2023-04-05 20:26:50 -0400

    Fixed CORS

--- Backend/api.py
+++ Backend/api.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
+
 import loadDatabase
 from Students import *
 from Spaces import *
@@ -7,6 +9,8 @@ from Company import *
 from Courses import *
 
 app = Flask(__name__)
+CORS(app)
+
 api = Api(app)
 
 
